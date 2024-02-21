b443621ea4a66a3ed94438a528ca6f83085f1655
AntonyMorozov <moroztoni91@mail.ru>
2024-02-21 10:29:33 +0400

    Initial commit

---
+++ src/main/java/by/javaguru/git/mergeexperience/Module1Servlet.java
@@ -0,0 +1,40 @@
+package by.javaguru.git.mergeexperience;
+
+import java.io.*;
+
+import by.javaguru.git.mergeexperience.topics.Module1Topics;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/module1")
+public class Module1Servlet extends HttpServlet {
+    private String message;
+    public void init() {
+        message = "Модуль 1";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+
+        out.println("<table>");
+        for (Module1Topics value : Module1Topics.values())
+            out.println("<tr><td>"
+                    + value.getOrder() + "</td><td>"
+                    + value.getTopic() + "</td><td>"
+                    + value.getDesc()
+                    + "</td><td><a href=''>Подробнее</a>"
+                    + "</td></tr>");
+
+        out.println("</table>");
+
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
